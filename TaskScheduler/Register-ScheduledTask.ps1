aad7a6b5c5fef4cc6e073fffc3ba93217805aaa9
shirokuman <fun-for-fan@outlook.com>
2018-11-17 21:34:09 +0900

    first commit

--- TaskScheduler/Register-ScheduledTask.ps1
+++ TaskScheduler/Register-ScheduledTask.ps1
@@ -1,7 +1,8 @@
 ﻿# # Variables
 $taskName = "DeleteFile"
+$psFile = "C:\OneDrive\DS218j\cloud\azure\windows-powershell\Remove-Item-specified-number-of-days.ps1"
 $excute = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-$action = New-ScheduledTaskAction -Execute $excute -Argument "-Command "C:\OneDrive\DS218j\cloud\azure\windows-powershell\Remove-Item-specified-number-of-days.ps1""
+$action = New-ScheduledTaskAction -Execute $excute -Argument "-Command $psFile"
 $trigger = New-ScheduledTaskTrigger -DaysInterval 1 -Daily -At "3:00 AM"
 
 # Create task scheduler
